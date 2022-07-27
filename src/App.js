ffb31047dfbdee4cc0cf99135837cfe5c56a0353
Bobbyshu <shuyunmu@163.com>
2022-07-27 01:42:31 -0400

    Final demo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
-
+import { unstable_HistoryRouter as HistoryRouter, Routes, Route } from 'react-router-dom'
+import { history } from './utils/history'
+import Layout from './pages/Layout'
+// import { Button } from 'antd'
+import Login from './pages/Login'
+import './App.css'
+import { AuthComponent } from '@/components/AuthComponent'
+import Publish from './pages/Publish'
+import Article from './pages/Article'
+import Home from './pages/Home'
 
 function App () {
   return (
-    <div className="App">
-      app
-    </div>
+    <HistoryRouter history={history}>
+      <div className="App">
+        <Routes>
+          <Route path='/' element={
+            <AuthComponent>
+              <Layout />
+            </AuthComponent>
+          }>
+            <Route index element={<Home />}></Route>
+            <Route path='article' index element={<Article />}></Route>
+            <Route path='publish' index element={<Publish />}></Route>
+          </Route>
+          <Route path='/login' element={<Login />}></Route>
+        </Routes>
+      </div>
+    </HistoryRouter>
   )
 }
 
